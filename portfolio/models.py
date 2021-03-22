fa46646a1ba9b8a83e5f1de36a68448740b4e066
Ishrak_Hussain <ishrakhussain30@gmail.com>
2021-03-22 14:14:29 +0600

    second iteration4

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Project(models.Model):
     title=models.CharField(max_length=100)
-    description=models.CharField(max_length=250)
+    description=models.CharField(max_length=150)
     image = models.ImageField(upload_to = 'portfolio/images/')
     url = models.URLField(blank=True) #blank = true means you can keep it blank if you want can be done in other fields
     
